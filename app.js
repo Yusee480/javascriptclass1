47dc26f9d08394de616609d0a49398db4faedeec
Yusee480 <adamuyusuf480@gmail.com>
2024-02-23 17:19:44 +0100

    new

---
+++ app.js
@@ -0,0 +1,27 @@
+alert('helloo');
+
+
+// console.log('Hi Habibullahi');
+
+
+// document.write('This is cool');
+
+
+// prompt('What is your age');#
+
+// var myName = 'Yusuf';
+
+// console.log('Welcome ' + myName);
+
+//data types
+// alert('hello world')
+// prompt('what is your name')
+// var myName = 'yusee'
+// console.log('what is my name ' + myName)
+
+// String
+// Number
+// Boolean
+// Array
+// Object
+
